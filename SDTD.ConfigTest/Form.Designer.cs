8bcfa71914b43fce13295aa3e41f1758c6876474
Samuel Holland <samuel@sholland.org>
2016-01-09 22:47:43 -0600

    Add simple program for testing configuration library

---
+++ SDTD.ConfigTest/Form.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace SDTD.ConfigTest
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // statusBox
+            // 
+            this.statusBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.statusBox.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusBox.Location = new System.Drawing.Point(0, 0);
+            this.statusBox.Multiline = true;
+            this.statusBox.Name = "statusBox";
+            this.statusBox.ReadOnly = true;
+            this.statusBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.statusBox.Size = new System.Drawing.Size(764, 521);
+            this.statusBox.TabIndex = 0;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 521);
+            this.Controls.Add(this.statusBox);
+            this.Name = "Form";
+            this.Text = "Configuration Library Test";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox statusBox;
+    }
+}
+
---
+++ SDTD.ConfigTest/Form.cs
@@ -0,0 +1,50 @@
+﻿using SDTD.Config;
+using System;
+using System.IO;
+using System.Xml.Linq;
+
+namespace SDTD.ConfigTest
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(Object sender, EventArgs e)
+        {
+            statusBox.AppendText(String.Format("Reading data from \"{0}\"...{1}", dataPath, Environment.NewLine));
+            XDocument blocksXML = XDocument.Load(dataPath + Path.DirectorySeparatorChar + "blocks.xml");
+            XDocument itemsXML = XDocument.Load(dataPath + Path.DirectorySeparatorChar + "items.xml");
+            XDocument materialsXML = XDocument.Load(dataPath + Path.DirectorySeparatorChar + "materials.xml");
+            XDocument recipesXML = XDocument.Load(dataPath + Path.DirectorySeparatorChar + "recipes.xml");
+
+            blocks = BlockCollection.Load(blocksXML);
+            statusBox.AppendText(String.Format("  Found {0} blocks.{1}", blocks.Count, Environment.NewLine));
+            items = ItemCollection.Load(itemsXML);
+            statusBox.AppendText(String.Format("  Found {0} items.{1}", items.Count, Environment.NewLine));
+            materials = MaterialCollection.Load(materialsXML);
+            statusBox.AppendText(String.Format("  Found {0} materials.{1}", materials.Count, Environment.NewLine));
+            recipes = RecipeCollection.Load(recipesXML);
+            statusBox.AppendText(String.Format("  Found {0} recipes.{1}", recipes.Count, Environment.NewLine));
+
+            statusBox.AppendText("XML for wood frame:" + Environment.NewLine);
+            statusBox.AppendText(blocks["woodFrame"].ToXElement().ToString());
+            statusBox.AppendText(Environment.NewLine);
+            statusBox.AppendText("XML for wall candle (has CanPickup property with param1):" + Environment.NewLine);
+            statusBox.AppendText(blocks["candleWall"].ToXElement().ToString());
+            statusBox.AppendText(Environment.NewLine);
+            statusBox.AppendText("XML for material of rebarFrame (extends solidRebarFrame):" + Environment.NewLine);
+            statusBox.AppendText(materials[blocks["rebarFrame"].GetProperty("Material")].ToXElement().ToString());
+            statusBox.AppendText(Environment.NewLine);
+        }
+
+        private String dataPath = @"C:\Program Files (x86)\Steam\steamapps\common\7 Days To Die\Data\Config";
+
+        private BlockCollection blocks;
+        private ItemCollection items;
+        private MaterialCollection materials;
+        private RecipeCollection recipes;
+    }
+}
