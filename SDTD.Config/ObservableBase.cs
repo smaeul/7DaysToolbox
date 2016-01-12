7b6fc103fff9d1311a8c5ef3ac50c2acc55ff12e
Samuel Holland <samuel@sholland.org>
2016-01-11 20:54:47 -0600

    Introduce generic base class for things and collections
    Remove recipes, as they are incompatible with it, and were
    stubs before. They will be re-added as a property of blocks.

---
+++ SDTD.Config/ObservableBase.cs
@@ -0,0 +1,75 @@
+﻿namespace SDTD.Config
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using System.Xml.Linq;
+
+    /// <summary>
+    /// Represents an item with modifiable properties, expressible as an XML element.
+    /// </summary>
+    interface IElement
+    {
+        void AddProperty(XElement property);
+        String GetProperty(String name);
+        Boolean SetProperty(String name, String value);
+        XElement ToXElement();
+    }
+
+    /// <summary>
+    /// Represents an observable object with a unique, immutable name and (optionally) ID.
+    /// </summary>
+    public abstract class ObservableBase : INotifyPropertyChanged
+    {
+        protected ObservableBase(UInt32? id, String name)
+        {
+            if (name == null) { throw new ArgumentNullException(nameof(name)); }
+            this.ID = id;
+            this.Name = name;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public UInt32? ID { get; }
+        public String Name { get; }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected Boolean SetField<U>(ref U field, U value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<U>.Default.Equals(field, value)) {
+                return false;
+            }
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// Represents an element in an <c>ObservableBaseCollection</c> of the same type.
+    /// </summary>
+    /// <typeparam name="T">The type of the element.</typeparam>
+    public abstract class ObservableBase<T> : ObservableBase
+        where T : ObservableBase<T>
+    {
+        protected static String _elementName = typeof(T).Name.ToLowerInvariant();
+
+        protected ObservableBase(ObservableBaseCollection<T> collection, UInt32? id, String name) : base(id, name)
+        {
+            if (collection == null) { throw new ArgumentNullException(nameof(collection)); }
+            this.Collection = collection;
+        }
+
+        public ObservableBaseCollection<T> Collection { get; }
+    }
+}
+
+namespace System.Runtime.CompilerServices
+{
+    sealed class CallerMemberNameAttribute : Attribute { }
+}
---
+++ SDTD.Config/ObservableBaseCollection.cs
@@ -0,0 +1,78 @@
+﻿namespace SDTD.Config
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Collections.Specialized;
+    using System.Xml.Linq;
+
+    /// <summary>
+    /// Represents a collection of XML elements (<c>IElement</c>).
+    /// </summary>
+    interface IElementCollection
+    {
+        void Load(XDocument document);
+        void Save(XDocument document);
+    }
+
+    /// <summary>
+    /// Represents an observable, indexed collection of observable elements.
+    /// </summary>
+    /// <typeparam name="T">The type of element in the collection.</typeparam>
+    public class ObservableBaseCollection<T> : KeyedCollection<String, T>, INotifyCollectionChanged
+        where T : ObservableBase<T>
+    {
+        protected static String _elementName = typeof(T).Name.ToLowerInvariant();
+
+        protected ObservableBaseCollection() : base(null, 0)
+        {
+            // No additional implementation.
+        }
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        public Boolean ContainsKey(String key)
+        {
+            return this.Dictionary.Keys.Contains(key);
+        }
+
+        internal void UpdateKey(T item)
+        {
+            base.ChangeItemKey(item, this.GetKeyForItem(item));
+        }
+
+        protected override void ClearItems()
+        {
+            base.ClearItems();
+            this.CollectionChanged?.Invoke(this,
+                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        protected override String GetKeyForItem(T item)
+        {
+            return item.Name;
+        }
+
+        protected override void InsertItem(Int32 index, T item)
+        {
+            base.InsertItem(index, item);
+            this.CollectionChanged?.Invoke(this,
+                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item, index));
+        }
+
+        protected override void RemoveItem(Int32 index)
+        {
+            T removedItem = this[index];
+            base.RemoveItem(index);
+            this.CollectionChanged?.Invoke(this,
+                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, removedItem, index));
+        }
+
+        protected override void SetItem(Int32 index, T item)
+        {
+            T oldItem = this[index];
+            base.SetItem(index, item);
+            this.CollectionChanged?.Invoke(this,
+                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, item, oldItem, index));
+        }
+    }
+}
